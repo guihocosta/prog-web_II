47c9d8d697cf4a7a5d655574ffb1613ad14602a9
Guilherme Costa <guiihocosta@gmail.com>
2022-09-20 17:19:52 -0300

    Atividade da Semana

---
+++ alterartitulo.js
@@ -0,0 +1,44 @@
+let tam = 2 
+let titulo 
+for (i = 1; i < 7; i++) {
+    titulo = document.querySelector(`h${i}`) 
+    if (titulo != null) {
+        f_criaBotao() 
+        break
+    }
+} 
+
+function f_criaBotao() {
+    const botaoMais = document.createElement(null) 
+    botaoMais.innerHTML = `<button id="botaomais"> Aumenta </button>` 
+    botaoMais.style.borderRadius = '10px'
+    botaoMais.style.width = '30px'
+
+    const botaoMenos = document.createElement(null) 
+    botaoMenos.innerHTML = `<button id="botaomenos"> Diminui </button>`
+    botaoMenos.style.borderRadius = '10px'
+    botaoMenos.style.width = '30px'
+    botaoMenos.style.marginLeft = '20px'
+
+    const br = document.createElement('br') 
+
+    titulo.parentNode.insertBefore(br, titulo.nextSibling)
+    titulo.parentNode.insertBefore(botaoMenos, titulo.nextSibling)
+    titulo.parentNode.insertBefore(botaoMais, titulo.nextSibling)
+} 
+
+const botaoMais = document.querySelector('#botaomais') 
+const botaoMenos = document.querySelector('#botaomenos') 
+
+botaoMais.addEventListener('click', function() {
+    if (tam < 5) {
+        tam += 0.5
+    } 
+    titulo.style.fontSize = `${tam}em`
+})
+botaoMenos.addEventListener('click', function() { 
+    if (tam > 2) {
+        tam -= 0.5
+    }
+    titulo.style.fontSize = `${tam}em`
+})
