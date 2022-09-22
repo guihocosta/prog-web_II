279057b6fb2b06cd2dbe68a657dc1eeb5bb94e44
Guilherme Costa <guiihocosta@gmail.com>
2022-09-22 16:32:15 -0300

    Update alterartitulo.js

--- alterartitulo.js
+++ alterartitulo.js
@@ -10,12 +10,12 @@ for (i = 1; i < 7; i++) {
 
 function f_criaBotao() {
     const botaoMais = document.createElement(null) 
-    botaoMais.innerHTML = `<button id="botaomais"> Aumenta </button>` 
+    botaoMais.innerHTML = `<button id="botaomais"> + </button>` 
     botaoMais.style.borderRadius = '10px'
     botaoMais.style.width = '30px'
 
     const botaoMenos = document.createElement(null) 
-    botaoMenos.innerHTML = `<button id="botaomenos"> Diminui </button>`
+    botaoMenos.innerHTML = `<button id="botaomenos"> - </button>`
     botaoMenos.style.borderRadius = '10px'
     botaoMenos.style.width = '30px'
     botaoMenos.style.marginLeft = '20px'
@@ -27,13 +27,13 @@ function f_criaBotao() {
     titulo.parentNode.insertBefore(botaoMais, titulo.nextSibling)
 
     botaoMais.addEventListener('click', function() {
-        if (tam < 5) {
+        if (tam < 6) {
             tam += 0.5
         } 
         titulo.style.fontSize = `${tam}em`
     })
     botaoMenos.addEventListener('click', function() { 
-        if (tam > 2) {
+        if (tam > 1) {
             tam -= 0.5
         }
         titulo.style.fontSize = `${tam}em`
